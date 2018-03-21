4db307cc5015ef1664df1c91666a203e5933f45a
David Morrissey <davemorrissey@gmail.com>
2018-03-21 16:37:26 +0000

    Experimenting with step styles and header

--- src/App.js
+++ src/App.js
@@ -32,28 +32,54 @@ class App extends Component {
         <div className="container">
           <div className="row">
             <div className="col-xs-12">
-              <h4>Input</h4>
-              <textarea
-                className="code"
-                type="text"
-                value={this.state.string}
-                onChange={this.handleChange}
-                rows="4"
-                placeholder="Type here">
-              </textarea>
-              <div className="result-info">{this.state.string.length} characters</div>
-              <hr/>
-              <h4>MD5</h4>
-              <div className="result-info">{digest.length()} bytes</div>
-              <div>(No preview)</div>
-              <hr/>
-              <h4>Bytes to hex</h4>
-              <div className="result-info">String, {hex.length} characters</div>
-              <code>{hex}</code>
-              <hr/>
-              <h4>Reverse</h4>
-              <div className="result-info">String, {hex.length} characters</div>
-              <code>{reverse}</code>
+              <div className="step step-input">
+                <h4>Input</h4>
+                <textarea
+                  className="code"
+                  type="text"
+                  value={this.state.string}
+                  onChange={this.handleChange}
+                  rows="4"
+                  placeholder="Type here">
+                </textarea>
+                <div className="result-info">{this.state.string.length} characters</div>
+                <div className="step-tail">
+                  <div className="step-tail-1"></div>
+                  <div className="step-tail-2"></div>
+                </div>
+              </div>
+              <div className="step step-transform">
+                <div className="step-top">
+                  <div className="step-top-1"></div>
+                </div>
+                <h4>MD5</h4>
+                <div className="result-info">{digest.length()} bytes</div>
+                <div>(No preview)</div>
+                <div className="step-tail">
+                  <div className="step-tail-1"></div>
+                  <div className="step-tail-2"></div>
+                </div>
+              </div>
+              <div className="step step-transform">
+                <div className="step-top">
+                  <div className="step-top-1"></div>
+                </div>
+                <h4>Bytes to hex</h4>
+                <div className="result-info">String, {hex.length} characters</div>
+                <code>{hex}</code>
+                <div className="step-tail">
+                  <div className="step-tail-1"></div>
+                  <div className="step-tail-2"></div>
+                </div>
+              </div>
+              <div className="step step-transform">
+                <div className="step-top">
+                  <div className="step-top-1"></div>
+                </div>
+                <h4>Reverse</h4>
+                <div className="result-info">String, {hex.length} characters</div>
+                <code>{reverse}</code>
+              </div>
             </div>
           </div>
         </div>
