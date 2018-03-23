f525914f8c2d37bd66e8620bab4380ee998a68fa
David Morrissey <davemorrissey@gmail.com>
2018-03-23 07:16:55 +0000

    Byte array default conversion to hex

--- src/pipeline/StepComponent.js
+++ src/pipeline/StepComponent.js
@@ -6,14 +6,15 @@ class StepComponent extends Component {
   render() {
     var step = this.props.step;
     var output = step.getOutput();
+    var message = null;
     var view = 'No preview available';
     var meta = 'Unknown type';
     if (output == null || output.data == null) {
       view = 'NULL';
       meta = 'null';
     } else if (output.type === 'ByteStringBuffer') {
-      view = 'No preview available for byte array';
-      console.log(output.data);
+      message = <div className="message"><span className="ion-md-information-circle"/> Showing byte array as hex (no separator, lower case) - add an encode step to see another representation</div>;
+      view = output.data.toHex();
       meta = 'Byte array, ' + output.data.length() + ' bytes';
     } else if (output.type === 'String') {
       view = output.data;
@@ -28,6 +29,7 @@ class StepComponent extends Component {
           <h4>{this.props.step.constructor.title}</h4>
         </div>
         <div className="step-body">
+          {message}
           <div className="data">{view}</div>
           <div className="meta">{meta}</div>
         </div>
