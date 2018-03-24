a6ebedf3f525e4ecbd56aba8a6cf29b9f163a543
David Morrissey <davemorrissey@gmail.com>
2018-03-24 08:52:09 +0000

    Step selector rewritten as nested SASS

--- src/pipeline/StepSelector.js
+++ src/pipeline/StepSelector.js
@@ -4,6 +4,7 @@ import Hex from './steps/encode/Hex';
 import Reverse from './steps/string/Reverse';
 import UpperCase from './steps/string/UpperCase';
 import { StepTop } from '../Common';
+import './StepSelector.css';
 
 class StepSelector extends Component {
 
@@ -19,24 +20,22 @@ class StepSelector extends Component {
     return (
       <div className="step-selector">
         <StepTop/>
-        <div className="step-body">
-          <div className="container-fluid">
-            <div className="row">
-              {
-                Object.keys(this.categories).map((name) => {
-                  return (<div key={name} className="col-md-3 category">
-                    <h4>{name.toUpperCase()}</h4>
-                    {
-                      this.categories[name].map((step, i) =>
-                        <button key={i} className="btn btn-inverse btn-block btn-sm"
-                                onClick={this.addStep.bind(this, step)}>{step.title}</button>
-                      )
-                    }
-                  </div>)
-                })
-              }
-            </div>
-          </div>
+        <div className="body">
+          {
+            Object.keys(this.categories).map((name) => {
+              return (<div key={name} className="category">
+                <div className="category-body">
+                  <h4>{name.toUpperCase()}</h4>
+                  {
+                    this.categories[name].map((step, i) =>
+                      <button key={i} className="btn btn-block btn-sm"
+                              onClick={this.addStep.bind(this, step)}>{step.title}</button>
+                    )
+                  }
+                </div>
+              </div>)
+            })
+          }
         </div>
       </div>
     );
