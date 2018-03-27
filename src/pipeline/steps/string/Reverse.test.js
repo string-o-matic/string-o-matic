6e210eab8801544245b3ef1b435e3bd1928e3883
David Morrissey <davemorrissey@gmail.com>
2018-03-27 06:52:20 +0100

    First test cases for steps

---
+++ src/pipeline/steps/string/Reverse.test.js
@@ -0,0 +1,26 @@
+import Reverse from './Reverse';
+import Data from '../../Data';
+import {StringType} from '../../Types';
+
+var step = new Reverse();
+
+// No tests for null or unsupported types - superclass rejects them.
+
+function expectResult(input, output) {
+  var result = step.calculate(Data.string(input));
+  expect(result.type).toBe(StringType);
+  expect(result.data).toBe(output);
+}
+
+test('empty', () => {
+  expectResult('', '');
+});
+
+test('string', () => {
+  expectResult('LOREM $^&* ipsum\t\r\n ', ' \n\r\tmuspi *&^$ MEROL');
+});
+
+test('mixed characters', () => {
+  expectResult('£āӨ抾Ⓡকę', 'ęকⓇ抾Өā£');
+});
+
---
+++ src/pipeline/steps/string/UpperCase.test.js
@@ -0,0 +1,34 @@
+import UpperCase from './UpperCase';
+import Data from '../../Data';
+import {StringType} from '../../Types';
+
+var step = new UpperCase();
+
+// No tests for null or unsupported types - superclass rejects them.
+
+function expectResult(input, output) {
+  var result = step.calculate(Data.string(input));
+  expect(result.type).toBe(StringType);
+  expect(result.data).toBe(output);
+}
+
+test('empty', () => {
+  expectResult('', '');
+});
+
+test('lower case ascii string', () => {
+  expectResult('lorem $^&* ipsum', 'LOREM $^&* IPSUM');
+});
+
+test('mixed case ascii string', () => {
+  expectResult('Dolor \r\n SIT Amet -=()!@<>', 'DOLOR \r\n SIT AMET -=()!@<>');
+});
+
+test('upper case ascii string', () => {
+  expectResult(' _CONSECTETUR ADIPISCING ,./;"', ' _CONSECTETUR ADIPISCING ,./;"');
+});
+
+test('mixed characters', () => {
+  expectResult('£āӨ抾Ⓡকę', '£ĀӨ抾ⓇকĘ');
+});
+
