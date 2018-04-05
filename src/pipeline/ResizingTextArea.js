f6d8aad7133aeef641a45a578c1cbadae4502cbc
David Morrissey <davemorrissey@gmail.com>
2018-04-05 07:38:00 +0100

    Prop type fix

--- src/pipeline/ResizingTextArea.js
+++ src/pipeline/ResizingTextArea.js
@@ -55,7 +55,7 @@ class ResizingTextArea extends Component {
 ResizingTextArea.propTypes = {
   readOnly: PropTypes.bool,
   value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
+  onChange: PropTypes.func,
   className: PropTypes.string
 };
 
