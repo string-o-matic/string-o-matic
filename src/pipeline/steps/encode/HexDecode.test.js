da5eabab775de0201f9b02f052efb4827e860634
David Morrissey <davemorrissey@gmail.com>
2018-04-05 18:09:09 +0100

    Set encoding properly for tests

--- src/pipeline/steps/encode/HexDecode.test.js
+++ src/pipeline/steps/encode/HexDecode.test.js
@@ -1,5 +1,4 @@
 import HexDecode from './HexDecode';
-import Globals from '../../../Globals';
 import Data from '../../Data';
 import {StringType} from '../../Types';
 
@@ -14,71 +13,71 @@ function expectResult(input, output) {
 }
 
 test('empty utf8', () => {
-  Globals.ENCODING = 'UTF-8';
+  step.setEncoding('UTF-8');
   expectResult('', '');
 });
 
 test('abc utf8', () => {
-  Globals.ENCODING = 'UTF-8';
+  step.setEncoding('UTF-8');
   expectResult('616263', 'abc');
 });
 
 test('space utf8', () => {
-  Globals.ENCODING = 'UTF-8';
+  step.setEncoding('UTF-8');
   expectResult('20', ' ');
 });
 
 test('pound utf8', () => {
-  Globals.ENCODING = 'UTF-8';
+  step.setEncoding('UTF-8');
   expectResult('c2a3', '\u00A3');
 });
 
 test('dash emoji utf8', () => {
-  Globals.ENCODING = 'UTF-8';
+  step.setEncoding('UTF-8');
   expectResult('f09f92a8', '\uD83D\uDCA8');
 });
 
 test('heavy black heart emoji utf8', () => {
-  Globals.ENCODING = 'UTF-8';
+  step.setEncoding('UTF-8');
   expectResult('e29da4', '\u2764');
 });
 
 test('red heart emoji utf8', () => {
-  Globals.ENCODING = 'UTF-8';
+  step.setEncoding('UTF-8');
   expectResult('e29da4efb88f', '\u2764\uFE0F');
 });
 
 test('empty utf16', () => {
-  Globals.ENCODING = 'UTF-16';
+  step.setEncoding('UTF-16');
   expectResult('', '');
 });
 
 test('abc utf16', () => {
-  Globals.ENCODING = 'UTF-16';
+  step.setEncoding('UTF-16');
   expectResult('006100620063', 'abc');
 });
 
 test('space utf16', () => {
-  Globals.ENCODING = 'UTF-16';
+  step.setEncoding('UTF-16');
   expectResult('0020', ' ');
 });
 
 test('pound utf16', () => {
-  Globals.ENCODING = 'UTF-16';
+  step.setEncoding('UTF-16');
   expectResult('00a3', '\u00A3');
 });
 
 test('dash emoji utf16', () => {
-  Globals.ENCODING = 'UTF-16';
+  step.setEncoding('UTF-16');
   expectResult('d83ddca8', '\uD83D\uDCA8');
 });
 
 test('heavy black heart emoji utf16', () => {
-  Globals.ENCODING = 'UTF-16';
+  step.setEncoding('UTF-16');
   expectResult('2764', '\u2764');
 });
 
 test('red heart emoji utf16', () => {
-  Globals.ENCODING = 'UTF-16';
+  step.setEncoding('UTF-16');
   expectResult('2764fe0f', '\u2764\uFE0F');
 });
