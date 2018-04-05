670098eca810d3d6c508375e5ce4850d908adb24
David Morrissey <davemorrissey@gmail.com>
2018-04-05 20:18:30 +0100

    PNG logo

--- src/chrome/Header.js
+++ src/chrome/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
   render() {
     return (
       <header>
-        <h1>string<span className="ion-ios-cog"/>matic</h1>
+        <h1 className="logo"><span>string-o-matic</span></h1>
         <h2>do things with strings</h2>
       </header>
     );
