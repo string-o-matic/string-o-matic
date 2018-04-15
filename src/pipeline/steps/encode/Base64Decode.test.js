b93065f396b8f5c98a7c613810a116178b060aaf
David Morrissey <davemorrissey@gmail.com>
2018-04-15 21:27:21 +0100

    Unit tests for UTF-16 b64 enc/dec

--- src/pipeline/steps/encode/Base64Decode.test.js
+++ src/pipeline/steps/encode/Base64Decode.test.js
@@ -2,7 +2,7 @@ import Base64EDecode from './Base64Decode';
 import Data from '../../Data';
 import {StringType} from '../../Types';
 
-var step;
+let step;
 
 beforeEach(() => {
   step = new Base64EDecode();
@@ -11,31 +11,67 @@ beforeEach(() => {
 // No tests for null or unsupported types - superclass rejects them.
 
 function expectResult(input, output) {
-  var result = step.calculate(Data.string(input));
+  const result = step.calculate(Data.string(input));
   expect(result.type).toBe(StringType);
   expect(result.data).toBe(output);
 }
 
-test('empty', () => {
+test('empty utf8', () => {
+  step.setEncoding('UTF-8');
   expectResult('', '');
 });
 
-test('f', () => {
+test('f utf8', () => {
+  step.setEncoding('UTF-8');
   expectResult('Zg==', 'f');
 });
 
-test('fo', () => {
+test('fo utf8', () => {
+  step.setEncoding('UTF-8');
   expectResult('Zm8=', 'fo');
 });
 
-test('foo', () => {
+test('foo utf8', () => {
+  step.setEncoding('UTF-8');
   expectResult('Zm9v', 'foo');
 });
 
-test('foobar', () => {
+test('foobar utf8', () => {
+  step.setEncoding('UTF-8');
   expectResult('Zm9vYmFy', 'foobar');
 });
 
-test('lines ignored', () => {
+test('lines ignored utf8', () => {
+  step.setEncoding('UTF-8');
   expectResult('Z\nm9v\nYm\nFy', 'foobar');
+});
+
+test('empty utf16', () => {
+  step.setEncoding('UTF-16');
+  expectResult('', '');
+});
+
+test('f utf16', () => {
+  step.setEncoding('UTF-16');
+  expectResult('AGY=', 'f');
+});
+
+test('fo utf16', () => {
+  step.setEncoding('UTF-16');
+  expectResult('AGYAbw==', 'fo');
+});
+
+test('foo utf16', () => {
+  step.setEncoding('UTF-16');
+  expectResult('AGYAbwBv', 'foo');
+});
+
+test('foobar utf16', () => {
+  step.setEncoding('UTF-16');
+  expectResult('AGYAbwBvAGIAYQBy', 'foobar');
+});
+
+test('lines ignored utf16', () => {
+  step.setEncoding('UTF-16');
+  expectResult('AGY\nAbwBv\nAGIA\nYQBy', 'foobar');
 });
--- src/pipeline/steps/encode/Base64Encode.test.js
+++ src/pipeline/steps/encode/Base64Encode.test.js
@@ -2,7 +2,7 @@ import Base64Encode from './Base64Encode';
 import Data from '../../Data';
 import {StringType} from '../../Types';
 
-var step;
+let step;
 
 beforeEach(() => {
   step = new Base64Encode();
@@ -11,32 +11,63 @@ beforeEach(() => {
 // No tests for null or unsupported types - superclass rejects them.
 
 function expectResult(input, output) {
-  var result = step.calculate(Data.string(input));
+  const result = step.calculate(Data.string(input));
   expect(result.type).toBe(StringType);
   expect(result.data).toBe(output);
 }
 
-test('empty', () => {
+test('empty utf8', () => {
+  step.setEncoding('UTF-8');
   expectResult('', '');
 });
 
-test('f', () => {
+test('f utf8', () => {
+  step.setEncoding('UTF-8');
   expectResult('f', 'Zg==');
 });
 
-test('fo', () => {
+test('fo utf8', () => {
+  step.setEncoding('UTF-8');
   expectResult('fo', 'Zm8=');
 });
 
-test('foo', () => {
+test('foo utf8', () => {
+  step.setEncoding('UTF-8');
   expectResult('foo', 'Zm9v');
 });
 
-test('foobar', () => {
+test('foobar utf8', () => {
+  step.setEncoding('UTF-8');
   expectResult('foobar', 'Zm9vYmFy');
 });
 
-test('line length', () => {
+test('line length utf8', () => {
+  step.setEncoding('UTF-8');
   step.setLineLength(2);
   expectResult('foobar', 'Zm\n9v\nYm\nFy');
+});
+
+test('empty utf16', () => {
+  step.setEncoding('UTF-16');
+  expectResult('', '');
+});
+
+test('f utf16', () => {
+  step.setEncoding('UTF-16');
+  expectResult('f', 'AGY=');
+});
+
+test('fo utf16', () => {
+  step.setEncoding('UTF-16');
+  expectResult('fo', 'AGYAbw==');
+});
+
+test('foo utf16', () => {
+  step.setEncoding('UTF-16');
+  expectResult('foo', 'AGYAbwBv');
+});
+
+test('foobar utf16', () => {
+  step.setEncoding('UTF-16');
+  expectResult('foobar', 'AGYAbwBvAGIAYQBy');
 });
